5f1976f1ca07adcc8c9e0385773e6464d3a72452
winstarDeveloper <prayushkale@gmail.com>
2024-11-18 01:48:59 +0530

    feat: add parallel exec to streamline matching with no less wait

--- run.py
+++ run.py
@@ -1,13 +1,42 @@
 import cv2
 import os, re, time
 import pytesseract
-import pyautogui
+# import pyautogui
+import ctypes
 import keyboard
 from PIL import Image
 from PIL import ImageGrab
 from concurrent.futures import ProcessPoolExecutor
+import threading
 
-pyautogui.FAILSAFE = False
+user32 = ctypes.windll.user32
+
+# Constants for mouse event
+MOUSEEVENTF_MOVE = 0x0001
+MOUSEEVENTF_LEFTDOWN = 0x0002
+MOUSEEVENTF_LEFTUP = 0x0004
+
+def set_timeout(func, delay_seconds, *args, **kwargs):
+    timer = threading.Timer(delay_seconds, func, args=args, kwargs=kwargs)
+    timer.daemon = True  # Thread will exit when main program exits
+    timer.start()
+    return timer
+
+def click_at(x, y):
+    # Set cursor position
+    user32.SetCursorPos(x, y)
+    time.sleep(0.05)
+    
+    # Simulate mouse left button down and up (click)
+    user32.mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+    time.sleep(0.05)  # Short delay for click effect
+    user32.mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+
+def run_click(x, y):
+    with ProcessPoolExecutor() as executor:
+        executor.submit(click_at, x, y)
+
+# pyautogui.FAILSAFE = False
 
 # Eng to Jpn dictionary
 eng_to_jpn = {
@@ -38,6 +67,7 @@ eng_to_jpn = {
     'baseball': 'youkoso',
     'four': 'yon',
     "o'clock": 'ji',
+    "o'clock": '',
     'noisy': 'urusai',
     'welcome': 'youkoso',
     'baseball': 'yakyuu',
@@ -49,10 +79,13 @@ eng_to_jpn = {
     'six': 'roku',
     'wallet': 'saifu',
     'tv': 'terebi',
+    'TV': 'terebi',
+    '': 'terebi',
     'news': 'nyuusu',
     'map': 'chizu',
     'curry': 'karee',
     'no': 'iie',
+    'no': '',
     'person': 'hito',
     'where': 'doko',
     'airport': 'kuukou',
@@ -125,39 +158,10 @@ eng_to_jpn = {
     'phone': 'denwa',
     'elevator': 'ereebeeta',
     'udon': 'udon',
+    'video': 'douga',
+    'outlet': 'konsento',
+    'judo': 'juudou',
 }
-
-# Speciify cooordinates of each box to click
-# click_A = [
-#     {'x': 318, 'y': 383}, # 1
-#     {'x': 318, 'y': 473}, # 2
-#     {'x': 318, 'y': 563}, # 3
-#     {'x': 318, 'y': 653}, # 4
-#     {'x': 318, 'y': 742}, # 5
-# ]
-# click_B = [
-#     {'x': 534, 'y': 383}, # 1
-#     {'x': 534, 'y': 473}, # 2
-#     {'x': 534, 'y': 563}, # 3
-#     {'x': 534, 'y': 653}, # 4
-#     {'x': 534, 'y': 742}, # 5
-# ]
-
-# # Specify the coordinates of the rectangle (left, top, right, bottom)
-# A = [
-#     [228, 357, 407, 412], # 1
-#     [228, 445, 407, 502], # 2
-#     [228, 537, 407, 592], # 3
-#     [228, 627, 407, 687], # 4
-#     [228, 718, 407, 774], # 5
-# ]
-# B = [
-#     [447, 357, 629, 385], # 1
-#     [447, 445, 629, 473], # 2
-#     [447, 537, 629, 563], # 3
-#     [447, 627, 629, 655], # 4
-#     [447, 718, 629, 745], # 5
-# ]
 
 
 # Speciify cooordinates of each box to click
@@ -178,18 +182,18 @@ click_B = [
 
 # Specify the coordinates of the rectangle (left, top, right, bottom)
 A = [
-    [1095, 520, 1270, 575], # 1
-    [1095, 610, 1270, 665], # 2
-    [1095, 700, 1270, 755], # 3
-    [1095, 790, 1270, 845], # 4
-    [1095, 880, 1270, 935], # 5
+    [1095, 520, 1280, 579], # 1
+    [1095, 615, 1280, 673], # 2
+    [1095, 710, 1280, 767], # 3
+    [1095, 805, 1280, 862], # 4
+    [1095, 898, 1280, 957], # 5
 ]
 B = [
-    [1310, 520, 1490, 545], # 1
-    [1310, 610, 1490, 640], # 2
-    [1310, 700, 1490, 730], # 3
-    [1310, 790, 1490, 820], # 4
-    [1310, 880, 1490, 910], # 5
+    [1321, 520, 1511, 549], # 1
+    [1321, 614, 1511, 644], # 2
+    [1321, 709, 1511, 738], # 3
+    [1321, 803, 1511, 832], # 4
+    [1321, 898, 1511, 929], # 5
 ]
 from difflib import SequenceMatcher
 
@@ -207,115 +211,166 @@ def find_best_match(dictionary, target_value, threshold=0.9):
     return matches[0] if matches else None
 
 left_row = {
-    0: None,
-    1: None,
-    2: None,
-    3: None,
-    4: None,
+    0: '',
+    1: '',
+    2: '',
+    3: '',
+    4: '',
 }
 right_row = {
-    0: None,
-    1: None,
-    2: None,
-    3: None,
-    4: None,
+    0: '',
+    1: '',
+    2: '',
+    3: '',
+    4: '',
 }
 
+previous_matches = {
+    0: '',
+    1: '',
+    2: '',
+    3: '',
+    4: '',
+}
 
 capture_count = 1
 
-def extract_data_per_row(row_idx):
+def extract_left_row_box(row_idx):
+    print(f"Extracting left row box {row_idx+1}")
+
     # Capture the screenshot of the rectangle
     left_row_screenshot = ImageGrab.grab((A[row_idx][0], A[row_idx][1], A[row_idx][2], A[row_idx][3]))
-    right_row_screenshot = ImageGrab.grab((B[row_idx][0], B[row_idx][1], B[row_idx][2], B[row_idx][3]))
-
+    
     # Save the screenshot
     left_row_screenshot.save(f"A{row_idx+1}.png")
-    right_row_screenshot.save(f"B{row_idx+1}.png")
-
+    
     # We then read the image with text
     left_row_image=cv2.imread(f'A{row_idx+1}.png')
-    right_row_image=cv2.imread(f'B{row_idx+1}.png')
-
+    
     # convert to grayscale image
     left_row_image_gray=cv2.cvtColor(left_row_image, cv2.COLOR_BGR2GRAY)
-    right_row_image_gray=cv2.cvtColor(right_row_image, cv2.COLOR_BGR2GRAY)
-
+    
     # use thresh or blur
     cv2.threshold(left_row_image_gray, 0,255,cv2.THRESH_BINARY| cv2.THRESH_OTSU)[1]
-    cv2.threshold(right_row_image_gray, 0,255,cv2.THRESH_BINARY| cv2.THRESH_OTSU)[1]
     # cv2.medianBlur(gray, 3)
 	
     # memory usage with image i.e. adding image to memory
-    left_row_filename = "{}.jpg".format(os.getpid())
+    left_row_filename = f"A{row_idx+1}_temp.jpg"
     cv2.imwrite(left_row_filename, left_row_image_gray)
     left_row_text = pytesseract.image_to_string(Image.open(left_row_filename), lang='eng')
     os.remove(left_row_filename)
 
-    right_row_filename = "{}.jpg".format(os.getpid())
+    # show the output images
+    # cv2.imshow("Left Output In Grayscale", left_row_image_gray)
+    
+    # add each word to the left_row and right_row dictionary
+    if left_row[row_idx] == '' or left_row[row_idx] == 'matched':
+        left_row[row_idx] = left_row_text.strip().replace('\n', '').replace(' ', '').lower()
+    
+    print(f"Extracted Left Row Box {row_idx+1}: {left_row[row_idx]}")
+    return left_row[row_idx]
+
+
+def extract_right_row_box(row_idx):
+    print(f"Extracting right row box {row_idx+1}")
+
+    # Capture the screenshot of the rectangle
+    right_row_screenshot = ImageGrab.grab((B[row_idx][0], B[row_idx][1], B[row_idx][2], B[row_idx][3]))
+    
+    # Save the screenshot
+    right_row_screenshot.save(f"B{row_idx+1}.png")
+    
+    # We then read the image with text
+    right_row_image=cv2.imread(f'B{row_idx+1}.png')
+    
+    # convert to grayscale image
+    right_row_image_gray=cv2.cvtColor(right_row_image, cv2.COLOR_BGR2GRAY)
+    
+    # use thresh or blur
+    cv2.threshold(right_row_image_gray, 0,255,cv2.THRESH_BINARY| cv2.THRESH_OTSU)[1]
+    # cv2.medianBlur(gray, 3)
+    
+    # memory usage with image i.e. adding image to memory
+    right_row_filename = f"B{row_idx+1}_temp.jpg"
     cv2.imwrite(right_row_filename, right_row_image_gray)
+
     right_row_text = pytesseract.image_to_string(Image.open(right_row_filename), lang='eng')
-    # print(right_row_text)
     os.remove(right_row_filename)
 
     # show the output images
-    # cv2.imshow("Left Output In Grayscale", left_row_image_gray)
     # cv2.imshow("Right Output In Grayscale", right_row_image_gray)
-
+    
     # add each word to the left_row and right_row dictionary
-    left_row[row_idx] = left_row_text.strip().replace('\n', '').replace(' ', '').lower()
-    right_row[row_idx] = right_row_text.strip().replace('\n', '').replace(' ', '').lower()
+    if right_row[row_idx] == '' or right_row[row_idx] == 'matched':
+        right_row[row_idx] = right_row_text.strip().replace('\n', '').replace(' ', '').lower()
+    
+    print(f"Extracted Right Row Box {row_idx+1}: {right_row[row_idx]}")
+    return right_row[row_idx]
 
-    # print(f"Row {row_idx}: {left_row[row_idx]}, {right_row[row_idx]}")
+
+def extract_data_per_row(row_idx):
+    left_row[row_idx] = extract_left_row_box(row_idx)
+    right_row[row_idx] = extract_right_row_box(row_idx)
 
     return left_row[row_idx], right_row[row_idx]
 
-    # wait 3 seconds
-    # cv2.waitKey(3000)
-    # close the window
-    # cv2.destroyAllWindows()
-
 if __name__ == '__main__':
+    with ProcessPoolExecutor() as executor:
+        results = executor.map(extract_data_per_row, range(5))
+
+    for row_idx, result in enumerate(results):
+        left_row[row_idx], right_row[row_idx] = result
+
+    print(left_row)
+    print(right_row)
 
     not_found = []
     while True:
-        with ProcessPoolExecutor() as executor:
-            results = executor.map(extract_data_per_row, range(5))
-
-        for row_idx, result in enumerate(results):
-            left_row[row_idx], right_row[row_idx] = result
-
-        # print(left_row)
-        # print(right_row)
-
         # break loop on pressing 'x'
         if keyboard.is_pressed('x'):
+            print('Not Found: ', not_found)
             break
+
+        last_clicked_x = -1
+        last_clicked_y = -1
+
+        print('Left Row: ', left_row)
+        print('Right Row: ', right_row)
 
         # find the matching words and click the corresponding left and then right box
         for row_idx in range(5):
-            if left_row[row_idx] in eng_to_jpn:
+            if left_row[row_idx] in eng_to_jpn and left_row[row_idx] != 'matched':
                 # find index of the matching word in right_row 
                 try:
-                    right_row_match = find_best_match(right_row, eng_to_jpn[left_row[row_idx]], 0.5)
+                    right_row_match = find_best_match(right_row, eng_to_jpn[left_row[row_idx]], 0.8)
                     if right_row_match:
                         match_idx = right_row_match[0]
 
+                        if previous_matches[row_idx] == f'{left_row[row_idx]}-{right_row[match_idx]}':
+                            continue
+
                         # click the corresponding left and right boxes
-                        pyautogui.click(click_A[row_idx]['x'], click_A[row_idx]['y'], button='left', interval=0.8)
-                        print(f'Clicked on A{row_idx+1}')
+                        click_at(click_A[row_idx]['x'], click_A[row_idx]['y'])
+                        # print(f'Clicked on A{row_idx+1}')
 
-                        time.sleep(0.3)
+                        click_at(click_B[match_idx]['x'], click_B[match_idx]['y'])
+                        # print(f'Clicked on B{match_idx+1}\n')
 
-                        pyautogui.click(click_B[match_idx]['x'], click_B[match_idx]['y'], button='left', interval=0.8)
-                        print(f'Clicked on B{match_idx+1}\n')
+                        last_clicked_x = click_B[match_idx]['x']
+                        last_clicked_y = click_B[match_idx]['y']
 
-                        time.sleep(0.3)
-
-                        # print(f"Row {row_idx}: {left_row[row_idx]} is matched with {right_row[match_idx]}")
+                        print(f"Row {row_idx}: {left_row[row_idx]} is matched with {right_row[match_idx]}")
                                         
+                        previous_matches[row_idx] = f'{left_row[row_idx]}-{right_row[match_idx]}'
+
                         # set right_row[match_idx] to '' to avoid matching again
-                        # right_row[match_idx] = ''
+                        right_row[match_idx] = 'matched'
+                        # also clear the left_row[row_idx] to avoid matching again
+                        left_row[row_idx] = 'matched'
+
+                        # start extracting data for matched box in parallel after 3 seconds
+                        set_timeout(extract_left_row_box, 1, row_idx)
+                        set_timeout(extract_right_row_box, 1, match_idx)
                     else:
                         pass
                         # print(f"Row {row_idx}: {left_row[row_idx]} is not found, available matches: {right_row}")
@@ -325,9 +380,11 @@ if __name__ == '__main__':
             else:
                 if left_row[row_idx] not in not_found:
                     not_found.append(left_row[row_idx])
-                    # print(f"Row {row_idx}: {left_row[row_idx]} is not matched, available matches: {right_row}")
+                    print(f"Row {row_idx}: {left_row[row_idx]} is not matched, available matches: {right_row}")
 
-            # next_row_index = (row_idx + 1) % 5
-            # pyautogui.click(click_A[next_row_index]['x'], click_A[next_row_index]['y'])
-            # print(f'\nClicked on A{next_row_index+1}\n')
-        print('--------------------------------------------')
+        # reset click by clicking again on the last clicked box
+        if last_clicked_x != -1 and last_clicked_y != -1:
+            click_at(last_clicked_x, last_clicked_y)
+
+        # time.sleep(1)
+        # print(f'--------------------------------------------')

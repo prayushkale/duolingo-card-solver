84cd2107ba6b616c13de70e8da7dcfe046b757e4
winstarDeveloper <prayushkale@gmail.com>
2024-12-15 19:54:24 +0530

    feat: code comments and video

---
+++ README.md
@@ -0,0 +1,27 @@
+# Install requirements
+pip install opencv-python
+pip install pytesseract
+pip install numpy
+pip install pillow
+
+# My System
+AMD Ryzen 5 with Nvidia 3060
+Windows 11
+
+Phone: Moto Edge 50 Pro, Smart Connect feature to Mirror Screen
+
+Note: click will not work on mac or linux systems
+
+# Setup 
+1. Use a good screen mirroring software for your phone
+2. Set the box coordinates in run.py (I used MPos to detect mouse coords)
+3. Try a test run
+4. Works well with big fonts and white background
+
+# Usage
+1. Start Duolingo Word Match
+2. Run 'python run.py'
+
+# Demo Video
+
+
--- run.py
+++ run.py
@@ -1,8 +1,8 @@
+# import libraries
 import cv2
 import os, re, time
 import pytesseract
-# import pyautogui
-# import easyocr
+import numpy as np
 import ctypes
 import keyboard
 from PIL import Image
@@ -13,16 +13,14 @@ from difflib import SequenceMatcher
 
 user32 = ctypes.windll.user32
 
-# Constants for mouse event
+# constants for mouse event
 MOUSEEVENTF_MOVE = 0x0001
 MOUSEEVENTF_LEFTDOWN = 0x0002
 MOUSEEVENTF_LEFTUP = 0x0004
 
-# reader = easyocr.Reader(['en'], gpu=True)
-
 # pyautogui.FAILSAFE = False
 
-# Eng to Jpn dictionary
+# eng to jpn dictionary
 eng_to_jpn = {
     'japan': 'nihon',
     'hello': 'konnichiwa',
@@ -75,7 +73,7 @@ eng_to_jpn = {
     'airport': 'kuukou',
     'white': 'shiroi',
     'tempura': 'tenpura',
-    'far': 'tooi',
+    'far': 'toi', # 'tooi',
     'mom': 'haha',
     'soccer': 'sakkaa',
     'sports': 'supootsu',
@@ -100,7 +98,7 @@ eng_to_jpn = {
     'tennis': 'tenisu',
     'yen': 'en',
     'yen': '',
-    'big': 'ooki',
+    'big': 'okii',
     'quiet': 'shizuka',
     'saturday': 'doyoubi',
     'eats': 'tabemasu',
@@ -111,7 +109,7 @@ eng_to_jpn = {
     'ramen': 'raamen',
     'skirt': 'sukaato',
     'smart': 'atamagaii',
-    'movies': 'eiga',
+    'movies': 'iga',
     'red': 'akai',
     'coat': 'kooto',
     'myson': 'musuko',
@@ -151,73 +149,109 @@ eng_to_jpn = {
     'tomorrow': 'ashita',
     'pasta': 'pasuta',
     'waltet': 'saifu',
-    'bye': 'ane',
+    'bye': 'jaane',
+    'vietnam': 'betonamu',
+    'comedy': 'komedi',
+    'fantasy': 'fantaji',
+    'mystery': 'misuterii',
+    'k-pop': 'poppu',
+    'action': 'akushon',
+    'desk': 'tsukue',
+    'spacious': 'hiroi',
+    'cramped': 'semai',
+    'kitchen': 'kicchin',
+    'probably': 'tabun',
+    'house': '',
+    'fridge': 'reizouko',
+    'long': 'nagai',
+    'fast': 'hayai',
+    'train': 'densha',
+    'station': 'eki',
+    'hour': 'jikan',
+    'bus': 'basu',
+    'about': 'gurai',
+    'next': 'tsugi',
 }
 
-# Speciify cooordinates of each box to click
+# specify cooordinates of each box to click
+# left rows
 click_A = [
-    {'x': 1275, 'y': 549}, # 1
-    {'x': 1275, 'y': 657}, # 2
-    {'x': 1275, 'y': 760}, # 3
-    {'x': 1275, 'y': 863}, # 4
-    {'x': 1275, 'y': 969}, # 5
+    {'x': 250, 'y': 500}, # A1
+    {'x': 250, 'y': 645}, # A2
+    {'x': 250, 'y': 780}, # A3
+    {'x': 250, 'y': 920}, # A4
+    {'x': 250, 'y': 1060}, # A5
 ]
+# right rows
 click_B = [
-    {'x': 1489, 'y': 545}, # 1
-    {'x': 1489, 'y': 650}, # 2
-    {'x': 1489, 'y': 756}, # 3
-    {'x': 1489, 'y': 860}, # 4
-    {'x': 1489, 'y': 966}, # 5
+    {'x': 550, 'y': 510}, # B1
+    {'x': 550, 'y': 640}, # B2
+    {'x': 550, 'y': 780}, # B3
+    {'x': 550, 'y': 920}, # B4
+    {'x': 550, 'y': 1060}, # B5
 ]
 
-# Specify the coordinates of the rectangle (left, top, right, bottom)
+# specify the coordinates of the rectangle [top_left_x, top_left_y, bottom_right_x, bottom_right_y]
+# left rows
 A = [
-    [1112, 508, 1273, 555], # 1
-    [1112, 612, 1273, 659], # 2
-    [1112, 719, 1273, 764], # 3
-    [1112, 824, 1273, 867], # 4
-    [1112, 927, 1273, 971], # 5
+    [60, 465, 250, 505], #A1
+    [60, 600, 250, 645], #A2
+    [60, 740, 250, 785], #A3
+    [60, 880, 250, 925], #A4
+    [60, 1015, 250, 1060], #A5
 ]
+# right rows
 B = [
-    [1340, 502, 1500, 531], # 1
-    [1340, 607, 1500, 633], # 2
-    [1340, 712, 1500, 740], # 3
-    [1340, 820, 1500, 845], # 4
-    [1340, 920, 1500, 951], # 5
+    [360, 450, 540, 480], # 1
+    [360, 575, 540, 620], # 2
+    [360, 715, 540, 760], # 3
+    [360, 850, 540, 898], # 4
+    [360, 990, 540, 1030], # 5
 ]
 
-# continue btn coordinates
-continue_btn_box = [1155, 1095, 1482, 1135]
+# continue btn coordinates [top_left_x, top_left_y, bottom_right_x, bottom_right_y]
+continue_btn_box = [40, 1220, 560, 1270]
+# continue btn click coordinates
 continue_btn = {
-    'x': 1310,
-    'y': 1110
+    'x': 300,
+    'y': 1240
 }
 
-def set_timeout(func, delay_seconds, *args, **kwargs):
-    def wrapper():
-        with ThreadPoolExecutor() as executor:
-            executor.submit(func, *args, **kwargs)
-
-    timer = threading.Timer(delay_seconds, wrapper)
-    timer.daemon = True  # Thread will exit when main program exits
-    timer.start()
-    return timer
 
 def click_at(x, y):
+    """
+    Simulates a mouse click at the given coordinates (x, y).
+
+    Args:
+        x (int): The x-coordinate.
+        y (int): The y-coordinate.
+    """
     # Set cursor position
     user32.SetCursorPos(x, y)
-    time.sleep(0.05)
+    time.sleep(0.01)
     
     # Simulate mouse left button down and up (click)
     user32.mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
-    time.sleep(0.05)  # Short delay for click effect
+    time.sleep(0.02)  # Short delay for click effect
     user32.mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
 
-def run_click(x, y):
-    with ProcessPoolExecutor() as executor:
-        executor.submit(click_at, x, y)
 
 def find_similar_values(dictionary, target_value, threshold=0.9):
+    """
+    Finds and returns a list of items from the given dictionary that have values
+    similar to the target value, based on a similarity threshold.
+
+    Args:
+        dictionary (dict): The dictionary to search for similar values.
+        target_value (str): The target value to compare against.
+        threshold (float): The minimum similarity ratio required for a match 
+                           (default is 0.9).
+
+    Returns:
+        list: A list of tuples, each containing a key, value, and similarity percentage,
+              sorted by similarity in descending order.
+    """
+
     matches = []
     
     for key, value in dictionary.items():
@@ -225,11 +259,27 @@ def find_similar_values(dictionary, target_value, threshold=0.9):
         if similarity >= threshold:
             matches.append((key, value, round(similarity * 100, 2)))
     return sorted(matches, key=lambda x: x[2], reverse=True)
+    
 
 def find_best_match(dictionary, target_value, threshold=0.9):
+    """
+    Finds and returns the best matching key, value pair from the given dictionary that has a value
+    similar to the target value, based on a similarity threshold.
+
+    Args:
+        dictionary (dict): The dictionary to search for similar values.
+        target_value (str): The target value to compare against.
+        threshold (float): The minimum similarity ratio required for a match (default is 0.9).
+
+    Returns:
+        tuple or None: A tuple containing the best matching key, value, and similarity percentage,
+                       or None if no match was found.
+    """
     matches = find_similar_values(dictionary, target_value, threshold)
     return matches[0] if matches else None
 
+
+# detected values
 left_row = {
     0: '',
     1: '',
@@ -245,6 +295,7 @@ right_row = {
     4: '',
 }
 
+# previous matches of rows
 previous_matches = {
     0: '',
     1: '',
@@ -252,6 +303,7 @@ previous_matches = {
     3: '',
     4: '',
 }
+# previous matches trial count
 previous_matches_trial_count = {
     0: 0,
     1: 0,
@@ -260,89 +312,99 @@ previous_matches_trial_count = {
     4: 0,
 }
 
+# detected continue button text
 continue_btn_text = ''
+
+# continue click count to check ongoing round
 continue_click_count = 1
 
 def extract_left_row_box(row_idx):
-    # print(f"Extracting left row box {row_idx+1}")
+    """
+    Extracts the text from the left row box at the given row index by taking a screenshot of the box, saving it, reading it with pytesseract, and returning the extracted text.
+    
+    Args:
+        row_idx (int): The row index of the box to extract.
+    
+    Returns:
+        str: The extracted text from the box.
+    """
 
     # Capture the screenshot of the rectangle
     left_row_screenshot = ImageGrab.grab((A[row_idx][0], A[row_idx][1], A[row_idx][2], A[row_idx][3]))
     
-    # Save the screenshot
-    # left_row_screenshot.save(f"A{row_idx+1}.jpg")
-    
-    # We then read the image with text
-    # left_row_image=cv2.imread(f'A{row_idx+1}.png')
-    
     # convert to grayscale image
-    # left_row_image_gray=cv2.cvtColor(left_row_image, cv2.COLOR_BGR2GRAY)
+    left_row_image=np.array(left_row_screenshot)
+    left_row_image_gray=cv2.cvtColor(left_row_image, cv2.COLOR_BGR2GRAY)
      
     # use thresh or blur
-    # cv2.threshold(left_row_image_gray, 0,255,cv2.THRESH_BINARY| cv2.THRESH_OTSU)[1]
-    # cv2.medianBlur(gray, 3)
+    (thresh, left_row_image_gray) = cv2.threshold(left_row_image_gray, 64, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    
+    # Save the screenshot
+    cv2.imwrite(f"A{row_idx+1}.jpg", left_row_image_gray)
 	
-    # memory usage with image i.e. adding image to memory
-    # left_row_filename = f"A{row_idx+1}_temp.jpg"
-    # cv2.imwrite(left_row_filename, left_row_image_gray)
-    # left_row_text = pytesseract.image_to_string(Image.open(f"A{row_idx+1}.jpg"), lang='eng')
-    left_row_text = pytesseract.image_to_string(left_row_screenshot, lang='eng')
-    # left_row_text = reader.readtext(f"A{row_idx+1}.jpg", detail=0)
-    # left_row_text = left_row_text[0] if left_row_text else ''
-    # os.remove(left_row_filename)
-
+    left_row_text = pytesseract.image_to_string(Image.open(f"A{row_idx+1}.jpg"), lang='eng')
+    # left_row_text = pytesseract.image_to_string(left_row_screenshot, lang='eng')
+    
     # show the output images
     # cv2.imshow("Left Output In Grayscale", left_row_image_gray)
     
-    # add each word to the left_row and right_row dictionary
-    # if left_row[row_idx] == '' or left_row[row_idx] == 'matched':
+    # add each word to the left_row dictionary, lowercase, remove newlines and spaces
     left_row[row_idx] = left_row_text.strip().replace('\n', '').replace(' ', '').lower()
     
-    # print(f"Extracted Left Row Box {row_idx+1}: {left_row[row_idx]}")
     return left_row[row_idx]
 
 
 def extract_right_row_box(row_idx):
-    # print(f"Extracting right row box {row_idx+1}")
+    """
+    Extracts the text from the right row box at the given row index by taking a screenshot of the box, saving it, reading it with pytesseract, and returning the extracted text.
+    
+    Args:
+        row_idx (int): The row index of the box to extract.
+    
+    Returns:
+        str: The extracted text from the box.
+    """
 
     # Capture the screenshot of the rectangle
     right_row_screenshot = ImageGrab.grab((B[row_idx][0], B[row_idx][1], B[row_idx][2], B[row_idx][3]))
     
     # Save the screenshot
-    # right_row_screenshot.save(f"B{row_idx+1}.jpg")
+    right_row_screenshot.save(f"B{row_idx+1}.jpg")
     
-    # We then read the image with text
-    # right_row_image=cv2.imread(f'B{row_idx+1}.png')
-    
-    # convert to grayscale image
-    # right_row_image_gray=cv2.cvtColor(right_row_image, cv2.COLOR_BGR2GRAY)
-    
+    right_row_image=np.array(right_row_screenshot)
+    right_row_image_gray=cv2.cvtColor(right_row_image, cv2.COLOR_BGR2GRAY)
+     
     # use thresh or blur
-    # cv2.threshold(right_row_image_gray, 0,255,cv2.THRESH_BINARY| cv2.THRESH_OTSU)[1]
-    # cv2.medianBlur(gray, 3)
+    (thresh, right_row_image_gray) = cv2.threshold(right_row_image_gray, 64, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
     
-    # memory usage with image i.e. adding image to memory
-    # right_row_filename = f"B{row_idx+1}_temp.jpg"
-    # cv2.imwrite(right_row_filename, right_row_image_gray)
-
-    # right_row_text = pytesseract.image_to_string(Image.open(f"B{row_idx+1}.jpg"), lang='eng')
-    right_row_text = pytesseract.image_to_string(right_row_screenshot, lang='eng')
-    # right_row_text = reader.readtext(f"B{row_idx+1}.jpg", detail=0)
-    # right_row_text = right_row_text[0] if right_row_text else ''
-    # os.remove(right_row_filename)
-
+    # save the screenshot
+    cv2.imwrite(f"B{row_idx+1}.jpg", right_row_image_gray)
+    
+    right_row_text = pytesseract.image_to_string(Image.open(f"B{row_idx+1}.jpg"), lang='eng')
+    # right_row_text = pytesseract.image_to_string(right_row_screenshot, lang='eng')
+    
     # show the output images
     # cv2.imshow("Right Output In Grayscale", right_row_image_gray)
     
-    # add each word to the left_row and right_row dictionary
-    # if right_row[row_idx] == '' or right_row[row_idx] == 'matched':
+    # add each word to the right_row dictionary, lowercase, remove newlines and spaces
     right_row[row_idx] = right_row_text.strip().replace('\n', '').replace(' ', '').lower()
     
-    # print(f"Extracted Right Row Box {row_idx+1}: {right_row[row_idx]}")
     return right_row[row_idx]
 
 
 def extract_data_per_row(row_idx):
+    """
+    Extracts data from both the left and right row boxes at the given row index.
+    It captures the text from each box, processes it, and stores it in the respective 
+    dictionaries: left_row and right_row.
+
+    Args:
+        row_idx (int): The row index for which data should be extracted.
+
+    Returns:
+        tuple: A tuple containing the extracted text from the left and right row boxes.
+    """
+
     left_row[row_idx] = extract_left_row_box(row_idx)
     right_row[row_idx] = extract_right_row_box(row_idx)
 
@@ -350,40 +412,63 @@ def extract_data_per_row(row_idx):
 
 
 def extract_continue_box():
+    """
+    Extracts text from the continue button by taking a screenshot of the button area,
+    saving it, and using pytesseract to read and return the extracted text.
+
+    Returns:
+        str: The extracted and processed text from the continue button.
+    """
+
     # Capture the screenshot of the rectangle
     continue_btn_screenshot = ImageGrab.grab((continue_btn_box[0], continue_btn_box[1], continue_btn_box[2], continue_btn_box[3]))
     
     # Save the screenshot
-    # continue_btn_screenshot.save("continue_btn.jpg")
+    continue_btn_screenshot.save("continue_btn.jpg")
 
+    # get the text
     continue_btn_text = pytesseract.image_to_string(continue_btn_screenshot, lang='eng')
-    continue_btn_text = continue_btn_text.strip().replace('\n', '').replace(' ', '').lower()
     
-    # print(f"Extracted Continue Button: {continue_btn_text}")
+    # add each word to the right_row dictionary, lowercase, remove newlines and spaces
+    continue_btn_text = continue_btn_text.strip().replace('\n', '').replace(' ', '').lower()
 
     return continue_btn_text
 
-not_found = [] 
+
+# maintain a list of not found words, to be shown at the end
+not_found = []
+
+# match count
 matched_count = {
     'count': 0
 }
+
+# continue click count, for round continuation
 continue_click_count = 1
 
 def click_on_matches():
+    """
+    Simulates a match by finding the matching words in the left and right row boxes and
+    clicking the corresponding left and then right box. It also keeps track of the matched
+    words to avoid matching them again.
+
+    Maintains a list of not found words, to be shown at the end.
+
+    It also starts extracting data for matched box in parallel after 3 seconds using
+    ThreadPoolExecutor.
+
+    :return: None
+    """
+
     last_clicked_x = -1
     last_clicked_y = -1
 
-    # print('Left Row: ', left_row)
-    # print('Right Row: ', right_row)
-
     # find the matching words and click the corresponding left and then right box
     for row_idx in range(5):
-        # print(f"Row {row_idx}: {left_row[row_idx]}")
         if left_row[row_idx] in eng_to_jpn and left_row[row_idx] != 'matched':
             # find index of the matching word in right_row 
-            # print("Finding best match: ", eng_to_jpn[left_row[row_idx]])
             try:
-                right_row_match = find_best_match(right_row, eng_to_jpn[left_row[row_idx]], 0.85)
+                right_row_match = find_best_match(right_row, eng_to_jpn[left_row[row_idx]], 0.6)
                 if right_row_match:
                     match_idx = right_row_match[0]
 
@@ -401,7 +486,7 @@ def click_on_matches():
                     last_clicked_x = click_B[match_idx]['x']
                     last_clicked_y = click_B[match_idx]['y']
 
-                    # print(f"Row {row_idx}: {left_row[row_idx]} is matched with {right_row[match_idx]}")
+                    print(f"Row {row_idx}: {left_row[row_idx]} is matched with {right_row[match_idx]}")
                                     
                     previous_matches[row_idx] = f'{left_row[row_idx]}-{right_row[match_idx]}'
                     previous_matches_trial_count[row_idx] = 0
@@ -412,10 +497,6 @@ def click_on_matches():
                     left_row[row_idx] = 'matched'
 
                     matched_count['count'] += 1
-
-                    # start extracting data for matched box in parallel after 3 seconds
-                    # set_timeout(extract_left_row_box, 2, row_idx)
-                    # set_timeout(extract_right_row_box, 2, match_idx)
                 else:
                     pass
                     # print(f"NM {row_idx}: {left_row[row_idx]}:{left_row}, {right_row}")
@@ -426,14 +507,11 @@ def click_on_matches():
         else:
             if left_row[row_idx] not in not_found:
                 not_found.append(left_row[row_idx])
-                # print(f"NF {row_idx}: {left_row[row_idx]}: {left_row}, {right_row}")
+                print(f"NF {row_idx}: {left_row[row_idx]}: {left_row}, {right_row}")
 
     # reset click by clicking again on the last clicked box
     if last_clicked_x != -1 and last_clicked_y != -1:
         click_at(last_clicked_x, last_clicked_y)
-
-    # time.sleep(1)
-    # print(f'--------------------------------------------')
 
 
 if __name__ == '__main__':
@@ -448,10 +526,8 @@ if __name__ == '__main__':
             for row_idx, result in enumerate(results):
                 left_row[row_idx], right_row[row_idx] = result
 
-            # print(f'{left_row}, {right_row}')
-
             # check if atleast 3 words in left_row and right_row are '' , if so then click on the continue button
-            if matched_count['count'] > 5 and len(list(filter(lambda x: x == '', left_row.values()))) >= 3 and len(list(filter(lambda x: x == '', right_row.values()))) >= 3:
+            if matched_count['count'] > 5 and len(list(filter(lambda x: x == '', left_row.values()))) >= 2 and len(list(filter(lambda x: x == '', right_row.values()))) >= 2:
                 # get continue button text
                 continue_btn_text = extract_continue_box()
                 
@@ -471,7 +547,7 @@ if __name__ == '__main__':
             
             continue_btn_text = extract_continue_box()
             
-            if not clicked_start and SequenceMatcher(None, continue_btn_text.lower(), 'start+75xp').ratio() >= 0.5:
+            if not clicked_start and SequenceMatcher(None, continue_btn_text.lower(), 'start+225xp').ratio() >= 0.5:
                 # click on the continue button
                 click_at(continue_btn['x'], continue_btn['y'])
                 continue_click_count += 1
@@ -491,6 +567,3 @@ if __name__ == '__main__':
             print("Not Found: ", not_found)
             print("matched_count: ", matched_count['count'])
             break
-
-    print(left_row)
-    print(right_row)       
